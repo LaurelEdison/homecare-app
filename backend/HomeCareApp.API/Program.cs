d1c7cbf39095afc6c53c7f9ae26a5a3b67d6608c
Earllld <arel.ferdinand@gmail.com>
2025-04-28 19:34:17 +0700

    Inject AppDBContext to Program.cs

--- backend/HomeCareApp.API/Program.cs
+++ backend/HomeCareApp.API/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using HomeCareApp.Infrastructure.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(opts =>
+    opts.UseNpgsql(connectionString)
+);
 
 var app = builder.Build();
 
